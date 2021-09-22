801c9bf2783df6866c1b71e6ed0bfa943153f34d
Kkr-Chowdary <konikinenikoteswararo@gamil.com>
2021-09-22 10:17:52 +0530

    comitting for firsttime

---
+++ ComCastSeleniumFrame/src/test/java/com/comcaste/ContactTest/ContactTest.java
@@ -0,0 +1,18 @@
+package com.comcaste.ContactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontactTest() {
+		System.out.println("execute contact");
+	}
+@Test
+public void createcontactwithorgTest() {
+	System.out.println(" createcontactwithorgTest");
+}
+@Test
+public void deletecontactTest() {
+	System.out.println("deletecontact");
+}
+}
---
+++ ComCastSeleniumFrame/src/test/java/com/comcaste/ContactTest/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author koteswar
+ *
+ */
+package com.comcaste.ContactTest;
